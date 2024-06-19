1e8a4cb7fc2ea048f14d5db2603035081bf5854f
insleker <bkinnightskytw@gmail.com>
2024-06-19 13:50:15 +0800

    feat: support conan2 building fix: upgrade catch2 to v2.13.10

--- conanfile.py
+++ conanfile.py
@@ -1,4 +1,8 @@
-from conans import ConanFile, CMake, tools
+# from conans import ConanFile, CMake
+from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout, CMakeDeps
+from conan.tools.build import check_min_cppstd, can_run
+from conan.errors import ConanInvalidConfiguration
 
 
 class FixedStringConan(ConanFile):
@@ -10,30 +14,49 @@ class FixedStringConan(ConanFile):
     description = "C++ library that provides a basic_fixed_string template that combines std::array fixed-size semantic and std::string semantic together"
     topics = ("cpp17", "string", "constexpr")
     settings = "os", "compiler", "build_type", "arch"
-    options = {"build_tests": [True, False],"build_examples":[True,False]}
-    default_options = {"build_tests": False,"build_examples":False}
-    generators = "cmake"
+    options = {"build_tests": [True, False], "build_examples": [True, False]}
+    default_options = {"build_tests": False, "build_examples": False}
 
     def source(self):
         self.run("git clone https://github.com/unterumarmung/fixed_string.git")
 
+    def validate(self):
+        check_min_cppstd(self, 17)
+        if self.options.build_examples:
+            raise ConanInvalidConfiguration(
+                "example is not (yet) available for fixed_string"
+            )
+
+    def layout(self):
+        cmake_layout(self)
+
+    def generate(self):
+        deps = CMakeDeps(self)
+        deps.generate()
+        tc = CMakeToolchain(self)
+
+        tc.variables["FIXED_STRING_OPT_BUILD_EXAMPLES"] = self.options.build_examples
+        tc.variables["FIXED_STRING_OPT_BUILD_TESTS"] = self.options.build_tests
+
+        tc.generate()
+
     def build(self):
         cmake = CMake(self)
         cmake.verbose = True
-        cmake.definitions["FIXED_STRING_OPT_BUILD_EXAMPLES"] = self.options.build_examples
-        cmake.definitions["FIXED_STRING_OPT_BUILD_TESTS"] = self.options.build_tests
-        
-        cmake.configure(source_folder="fixed_string")
+        cmake.configure()
         cmake.build()
-        if self.options.build_tests:
+        if self.options.build_tests and (
+            not self.conf.get("tools.build:skip_test", default=False) and can_run(self)
+        ):
             print("Running tests...")
             cmake.test()
-
 
     def package(self):
         self.copy("*.h", dst="include", src="fixed_string/include")
         self.copy("*.hpp", dst="include", src="fixed_string/include")
-        
+
     def package_info(self):
         self.cpp_info.libs = ["fixed_string"]
 
+    def package_id(self):
+        self.info.clear()
